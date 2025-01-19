1e96791b43bbbb9a731d517c71694be1533e2e66
Kristina Faye <hellokristinafaye@gmail.com>
2025-01-18 19:22:09 -0800

    OK looks like we can't get webkit animation to pause b/c it's deprecated and won't really work in most browsers?  apparently not in Chrome, so...

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Hello world!");
+var myImg = document.getElementById('myImg');
+
+myImg.onclick = function () {
+    if (clouds.style.webkitAnimationPlayState == "paused") {
+        clouds.style.webkitAnimationPlayState == "running";
+        airplane.style.webkitAnimationPlayState == "running";
+        hotairballoon.style.webkitAnimationPlayState == "running";
+        console.log('run')
+    } else {
+        clouds.style.webkitAnimationPlayState == "paused";
+        airplane.style.webkitAnimationPlayState == "paused";
+        hotairballoon.style.webkitAnimationPlayState == "paused";
+        console.log('pause')
+    }
+}
